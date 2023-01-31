3a97c3ff84a908fb9d4fa874807b58cbf6e1d9a9
Hugo Rodrigues Mota <hugomota.iblue@ciclic.com.br>
2023-01-31 15:38:45 -0300

    create project teste

---
+++ src/layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="container-content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+.container {
+  @apply mx-auto max-w-7xl px-4 sm:px-6 lg:px-8;
+
+  &-content {
+    @apply mx-auto max-w-3xl;
+  }
+}
+</style>
